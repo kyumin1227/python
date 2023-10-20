515163b9bd3e56596f488fe54263cdb72eff7d60
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-10-21 01:18:18 +0900

    first commit

---
+++ 1_프로그래밍-시작하기-in-Python/함수 생성.py
@@ -0,0 +1,5 @@
+def cafe_mocha_recipe():
+    print("1. 준비된 컵에 초코 소스를 넣는다.\n2. 에스프레소를 추출하고 잔에 부어 준다.\n3. 초코 소스와 커피를 잘 섞어 준다.\n4. 거품기로 우유 거품을 내고, 잔에 부어 준다.\n5. 생크림을 얹어 준다.")
+    
+cafe_mocha_recipe()
+cafe_mocha_recipe()
---
+++ 2_프로그래밍-핵심-개념-in-Python/format.py
@@ -0,0 +1,9 @@
+year = 2023
+month = 10
+day = 20
+
+print("오늘은 {}년 {}월 {}일입니다.".format(year, month, day))
+
+num_1 = 1
+num_2 = 3
+print("{0} 나누기 {1}은 {2:.2f}입니다.".format(num_1, num_2, num_1 / num_2))

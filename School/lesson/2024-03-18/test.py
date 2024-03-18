d95f6c3ac2956f48f5629fd6d62f8505056a20c5
kyumin1227 <kyumin12271227@gmail.com>
2024-03-18 17:18:27 +0900

    Programming lesson

---
+++ School/lesson/2024-03-18/test.py
@@ -0,0 +1,8 @@
+# "hello world" 출력
+print("hello world")
+
+'''
+파이썬에서 여러 줄
+주석 처리 할 경우
+위 Notation을 사용합니다.
+'''
---
+++ School/report/2024-03-18/lab3.py
@@ -0,0 +1,8 @@
+# 연도 입력 받기
+year_now = int(input("현재 연도를 입력하세요: "))
+
+# 태어난 연도 입력 받기
+year_birth = int(input("태어난 연도를 입력하세요: "))
+
+# 나이 계산 후 출력
+print(f"당신의 나이는 {year_now - year_birth}살 입니다.")

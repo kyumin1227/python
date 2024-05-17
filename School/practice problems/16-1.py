9f292a1a7e742cc78664e243f1219bd10a8ea29f
kyumin1227 <kyumin12271227@gmail.com>
2024-05-17 15:51:02 +0900

    2024.05.17 Practice Problem Fix & add Comment

--- School/practice problems/16-1.py
+++ School/practice problems/16-1.py
@@ -1,5 +1,6 @@
 import random
 
+# 랜덤한 수 세개를 중복없이 뽑는 함수
 def getComputerNumber():
     number_list = []
 
@@ -14,6 +15,7 @@ def getComputerNumber():
 
     return number_list
 
+# Strike와 Ball을 계산하는 함수
 def getStrikeAndBall(argList):
     strike = 0
     ball = 0
@@ -31,28 +33,33 @@ def getStrikeAndBall(argList):
 
 computer_list = getComputerNumber()
 win = False
-strikeOut = 0
+out = 0
 
+# 5번 실행
 for i in range(5):
+    # 숫자를 입력 받아 계산
     user_list = list(map(int, input(f"시도 {i + 1}: 입력한 숫자 - ").split()))
     strike, ball = getStrikeAndBall(user_list)
-    out = 3 - strike - ball
 
-    print(f"결과: {strike} Strike, {ball} Ball, {out} Out")
+    if strike + ball == 0:
+        out += 1
+
+    print(f"결과: {strike} Strike, {ball} Ball, {out} Out" if out > 0 else f"결과: {strike} Strike, {ball} Ball")
     
+    # 종료 조건
     if strike == 3:
         win = True
         break
 
-    if out == 3:
-        strikeOut += 1
-
-    if strikeOut == 2:
+    if out == 2:
         break
 
+# 종료 시 결과 확인
 if win:
     print("\n게임 종료: 승리")
+elif out == 2:
+    print("\n게임 종료: 패배 (스트라이크 아웃)")
 else:
-    print("게임 종료: 패배 (시도 횟수 5회 초과)")
+    print("\n게임 종료: 패배 (시도 횟수 5회 초과)")
 
 print(f"정답: {" ".join(map(str, computer_list))}")

bf713f26a1cf3ae0d35af50a98464852b3a4bf3a
kyumin1227 <kyumin12271227@gmail.com>
2024-06-19 16:03:48 +0900

    Practice Problem

--- School/practice problems/20-3.py
+++ School/practice problems/20-3.py
@@ -1,15 +1,22 @@
 import random
 
-# 1번의 입력 값 검증
-def check_one(arg_list):
-    for num in arg_list:
-        if num < 2 or num > 9:
+# 입력 값 검증
+def check(mode, arg_num):
+    if mode == 1:
+        for num in arg_num:
+            if num < 2 or num > 9:
+                return False
+            
+        if len(arg_num) > 2 or (len(arg_num) > 1 and arg_num[0] > arg_num[1]):
             return False
         
-    if len(arg_list) > 2 or (len(arg_list) > 1 and arg_list[0] > arg_list[1]):
-        return False
+        return True
     
-    return True
+    elif mode == 2:
+        if arg_num != 2 and arg_num != 3:
+            return False
+
+        return True
 
 # 1번의 출력
 def result_one(arg_start, arg_last = 0):
@@ -19,13 +26,6 @@ def result_one(arg_start, arg_last = 0):
         for j in range(1, 10):
             print(f"{i} * {j} = {i * j}")
         print()
-
-# 2번의 입력 값 검증
-def check_two(arg_num):
-    if arg_num != 2 and arg_num != 3:
-        return False
-
-    return True
 
 # 2번의 출력
 def result_two(arg_num):
@@ -55,14 +55,13 @@ while True:
     if not (user_select >= 1 and user_select <= 3):
         print("1~3 사이의 값을 입력하세요")
         continue
-
     
     if user_select == 1:
         while True:
             user_select_one = input("출력할 구구단을 아래 형식으로 입력하세요 (예: 2, 2~5)\n")
             user_select_one = list(map(int, user_select_one.split("~")))
             
-            if check_one(user_select_one):
+            if check(1, user_select_one):
                 result_one(*user_select_one)
                 break
             else:
@@ -72,7 +71,7 @@ while True:
         while True:
             user_select_two = int(input("삼각형의 높이 줄 수를 입력하세요(2 이상 3이하"))
 
-            if check_two(user_select_two):
+            if check(2, user_select_two):
                 result_two(user_select_two)
                 break
             else:

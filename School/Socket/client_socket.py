eda97fa0dccc19bd2c3052ab383ec183d12448cf
kyumin1227 <kyumin12271227@gmail.com>
2025-02-07 17:51:15 +0900

    School lesson (Socket with Thread)

--- School/Socket/client_socket.py
+++ School/Socket/client_socket.py
@@ -1,5 +1,4 @@
 import socket
-import time
 
 # 소켓 생성 (TCP or UDP, IP 주소 버전: v4 or v6)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,6 +6,13 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect (서버 주소, 서버 포트)
 client_socket.connect(("127.0.0.1", 5500))
 while True:
-    client_socket.sendall("123".encode("utf-8"))
+    send_msg = input("> ")
+
+    if send_msg == "Q":
+        break
+
+    client_socket.sendall(send_msg.encode("utf-8"))
     print(client_socket.recv(1024).decode("utf-8"))
-    time.sleep(3)
+
+print("종료 요청")
+client_socket.close()
--- School/Socket/server_socket.py
+++ School/Socket/server_socket.py
@@ -1,4 +1,19 @@
 import socket
+import threading
+
+def handler_client(client_socket: socket.socket, client_address: tuple[str, int]) -> None:
+    """클라이언트 마다 쓰레드를 통해 동작하는 함수"""
+    while True:
+        recv_data = client_socket.recv(1024).decode("utf-8")
+
+        if not recv_data:
+            print(f"[{client_address}]: 클라이언트의 연결이 종료 되었습니다.")
+            break
+
+        print(f"[{client_address}]: {recv_data}")
+        client_socket.sendall(recv_data.encode("utf-8"))
+
+    client_socket.close()
 
 # 소켓 생성 (TCP or UDP, IP 주소 버전: v4 or v6)
 # TCP: socket.SOCK_STREAM
@@ -11,13 +26,11 @@ server_socket.bind(("127.0.0.1", 5500))
 # listen(큐의 개수)
 server_socket.listen(5)
 
-# accept(), 사용자로부터 연결 요청을 받았을 때 새로운 소켓 생성
-# blocking 함수
-client_socket, client_address = server_socket.accept()
-while True:
-    recv_data = client_socket.recv(1024).decode("utf-8")
-    print(type(recv_data))
-    print(recv_data)
-    client_socket.sendall(recv_data.encode("utf-8"))
+threading.Thread
 
-print("Hello")
+while True:
+    # accept(), 사용자로부터 연결 요청을 받았을 때 새로운 소켓 생성
+    # blocking 함수
+    client_socket, client_address = server_socket.accept()
+    client_thread = threading.Thread(target=handler_client, args=(client_socket, client_address))
+    client_thread.start()

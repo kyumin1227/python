f9ed0dabdec2139db2e043bd672a492bd75a9d3f
kyumin1227 <kyumin12271227@gmail.com>
2025-09-18 18:00:08 +0900

    School lesson (oop)

---
+++ School/oop/09-18/1.py
@@ -0,0 +1,18 @@
+class Bar:
+    def __init__(self):
+        pass
+
+    def instance_method(self):
+        pass
+
+    @classmethod
+    def class_method(cls):
+        pass
+
+    def static_method():
+        pass
+
+obj = Bar()
+
+obj1 = Bar()
+obj2 = Bar()
